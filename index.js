51562b0e94deb4ce24457db8ca5ccf7602833105
Felipe Pavan <pavan.fhw@gmail.com>
2018-02-28 10:38:20 -0300

    event adder

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.post('/addFriend', (req, res) => {
 
 app.post('/events', (req, res) => {
     console.log("event inserted");
-
+    console.log(req.body);
     if (!req.body.name || !req.body.location || !req.body.timeStart || !req.body.peopleInvited) {
         res.status(400).send({ 'error': 'Preencha todos os campos obrigatorios' });
         return;
